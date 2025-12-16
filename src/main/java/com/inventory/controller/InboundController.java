a1c97e83e391047ab8f0d820691004828e069291
Deriou <3043890640@qq.com>
2025-12-16 15:48:31 +0800

    进货Controller接口

--- src/main/java/com/inventory/controller/InboundController.java
+++ src/main/java/com/inventory/controller/InboundController.java
@@ -1,7 +1,12 @@
 package com.inventory.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.inventory.entity.Inbound;
+import com.inventory.service.IInboundService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * <p>
@@ -14,5 +19,42 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/inbound")
 public class InboundController {
+    @Autowired
+    private IInboundService inboundService;
 
+    //提交进货单接口
+    @PostMapping("/add")
+    public Map<String,Object> add(@RequestBody Inbound inbound){
+        Map<String,Object> result = new HashMap<>();
+        try {
+            inboundService.createInbound(inbound);
+            result.put("success",true);
+            result.put("msg","下单成功");
+        }catch (Exception e){
+            result.put("success",false);
+            result.put("msg","下单失败："+e.getMessage());
+        }
+        return result;
+    }
+
+    //确认收货接口
+    @PostMapping("/confirm/{id}")
+    public Map<String,Object> confirm(@PathVariable long id){
+        Map<String,Object> result=new HashMap<>();
+        try{
+            inboundService.confirmInbound(id);
+            result.put("success",true);
+            result.put("msg","入库成功");
+        }catch (Exception e){
+            result.put("success",false);
+            result.put("msg","入库失败："+e.getMessage());
+        }
+        return result;
+    }
+
+    //查询列表
+    @GetMapping("/list")
+    public Object list(){
+        return inboundService.list();
+    }
 }
