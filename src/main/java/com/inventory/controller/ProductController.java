037c6836d50b325c6feb55d7528d0fc5d7a33043
Deriou <3043890640@qq.com>
2025-12-17 16:28:39 +0800

    ProductController的add接口配置

--- src/main/java/com/inventory/controller/ProductController.java
+++ src/main/java/com/inventory/controller/ProductController.java
@@ -1,7 +1,15 @@
 package com.inventory.controller;
 
+import com.inventory.entity.Product;
+import com.inventory.service.IProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * <p>
@@ -14,5 +22,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/product")
 public class ProductController {
+    @Autowired
+    private IProductService productService;
 
+    @PostMapping("/add")
+    public Map<String, Object> addProduct(@RequestBody Product product) {
+        Map<String, Object> result = new HashMap<>();
+        boolean success = productService.save(product);
+        result.put("success", success);
+        return result;
+    }
 }
